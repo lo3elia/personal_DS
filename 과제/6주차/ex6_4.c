5b14ae4628f1986f99fb6f89599a70af2484f56d
lo3elia <96883999+lo3elia@users.noreply.github.com>
2026-04-15 01:57:43 +0900

    Add files via upload

---
+++ 과제/6주차/ex6_4.c
@@ -0,0 +1,22 @@
+#include "deQue.h"
+
+int main() {
+	DQueType* DQ1 = createDQue();  // µ¥Å© »ý¼º
+	element data;
+	printf("\n ***** µ¥Å© ¿¬»ê ***** \n");
+	printf("\n front »ðÀÔ A>> ");  insertFront(DQ1, 'A');  printDQ(DQ1);
+	printf("\n front »ðÀÔ B>> ");  insertFront(DQ1, 'B');  printDQ(DQ1);
+	printf("\n rear  »ðÀÔ C>> ");  insertRear(DQ1, 'C');  printDQ(DQ1);
+	printf("\n front »èÁ¦  >> ");  data = deleteFront(DQ1);    printDQ(DQ1);
+	printf("\t»èÁ¦ µ¥ÀÌÅÍ: %c", data);
+	printf("\n rear  »èÁ¦  >> ");  data = deleteRear(DQ1);     printDQ(DQ1);
+	printf("\t\t»èÁ¦ µ¥ÀÌÅÍ: %c", data);
+	printf("\n rear  »ðÀÔ D>> ");  insertRear(DQ1, 'D');  printDQ(DQ1);
+	printf("\n front »ðÀÔ E>> ");  insertFront(DQ1, 'E');  printDQ(DQ1);
+	printf("\n front »ðÀÔ F>> ");  insertFront(DQ1, 'F');  printDQ(DQ1);
+
+	data = peekFront(DQ1);  printf("\n peek Front item : %c \n", data);
+	data = peekRear(DQ1);  printf(" peek Rear item : %c \n", data);
+
+	getchar();  return 0;
+}
